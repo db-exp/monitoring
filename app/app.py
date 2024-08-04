4f73209657ee7b48f142739b0411f6dd96f5bee8
db-exp <38574645+db-exp@users.noreply.github.com>
2024-08-04 18:44:51 +0300

    init

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(
+    app.wsgi_app, {"/metrics": make_wsgi_app()}
+)
+
+@app.route("/")
+@app.route("/up")
+def up():
+    return "I am running!"
